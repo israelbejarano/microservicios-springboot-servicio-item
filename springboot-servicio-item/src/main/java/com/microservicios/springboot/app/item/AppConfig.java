c3d1b921e2c70a557473648f7ec6cac35ae02b7a
israel <israel.bejarano.a@gmail.com>
2019-11-27 16:21:44 +0100

    configuracion propia para config Bean RestTemplate

---
+++ springboot-servicio-item/src/main/java/com/microservicios/springboot/app/item/AppConfig.java
@@ -0,0 +1,16 @@
+// @autor: Israel Bejarano
+package com.microservicios.springboot.app.item;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+	
+	@Bean
+	public RestTemplate registrarRestTemplate() {
+		return new RestTemplate();
+	}
+
+}
