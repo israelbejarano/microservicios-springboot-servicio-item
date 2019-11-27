75108998e11631f6a6d5a7233ef54864e0aa9cd2
israel <israel.bejarano.a@gmail.com>
2019-11-27 16:22:41 +0100

    add documentacion

--- springboot-servicio-item/src/main/java/com/microservicios/springboot/app/item/AppConfig.java
+++ springboot-servicio-item/src/main/java/com/microservicios/springboot/app/item/AppConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+// con esta clase y este bean podemos conectarnos a otros microservicios
+
 @Configuration
 public class AppConfig {
 	
