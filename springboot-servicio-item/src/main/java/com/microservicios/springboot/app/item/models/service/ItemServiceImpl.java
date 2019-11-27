0c21622e643471061213a29f9101608dbff60595
israel <israel.bejarano.a@gmail.com>
2019-11-27 16:21:22 +0100

    implementacion servicio item

---
+++ springboot-servicio-item/src/main/java/com/microservicios/springboot/app/item/models/service/ItemServiceImpl.java
@@ -0,0 +1,25 @@
+// @autor: Israel Bejarano
+package com.microservicios.springboot.app.item.models.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.microservicios.springboot.app.item.models.Item;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+	@Override
+	public List<Item> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item findById(Long id, Integer cantidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
