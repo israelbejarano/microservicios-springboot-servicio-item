9843862d98a03f48d724bb8cad5856b6106704e7
israel <israel.bejarano.a@gmail.com>
2019-11-27 16:29:32 +0100

    WIP implementando la conexion al otro microservicio

--- springboot-servicio-item/src/main/java/com/microservicios/springboot/app/item/models/service/ItemServiceImpl.java
+++ springboot-servicio-item/src/main/java/com/microservicios/springboot/app/item/models/service/ItemServiceImpl.java
@@ -1,19 +1,27 @@
 // @autor: Israel Bejarano
 package com.microservicios.springboot.app.item.models.service;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.microservicios.springboot.app.item.models.Item;
+import com.microservicios.springboot.app.item.models.Producto;
 
 @Service
 public class ItemServiceImpl implements ItemService {
+	
+	@Autowired
+	private RestTemplate clienteRest;
 
 	@Override
 	public List<Item> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Producto> productos = Arrays.asList(clienteRest.getForObject("http://localhost:8001/listar", Producto[].class));
+		return productos.stream().map(p -> new Item(p, 1)).collect(Collectors.toList());
 	}
 
 	@Override
