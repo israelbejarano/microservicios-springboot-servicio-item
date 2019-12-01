468f3c84315b601d86aaba22dff99fcaa84e3ef4
israel <israel.bejarano.a@gmail.com>
2019-12-01 12:42:26 +0100

    add nombre del bean

--- springboot-servicio-item/src/main/java/com/microservicios/springboot/app/item/AppConfig.java
+++ springboot-servicio-item/src/main/java/com/microservicios/springboot/app/item/AppConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfig {
 	
-	@Bean
+	@Bean("clienteRest")
 	public RestTemplate registrarRestTemplate() {
 		return new RestTemplate();
 	}
