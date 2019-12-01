2af332402a2e23656644fe2f17dc87600125bc3f
israel <israel.bejarano.a@gmail.com>
2019-12-01 12:47:20 +0100

    findById implementacion

--- springboot-servicio-item/src/main/java/com/microservicios/springboot/app/item/models/service/ItemServiceImpl.java
+++ springboot-servicio-item/src/main/java/com/microservicios/springboot/app/item/models/service/ItemServiceImpl.java
@@ -2,7 +2,9 @@
 package com.microservicios.springboot.app.item.models.service;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +28,10 @@ public class ItemServiceImpl implements ItemService {
 
 	@Override
 	public Item findById(Long id, Integer cantidad) {
-		// TODO Auto-generated method stub
-		return null;
+		Map<String, String> pathVariables = new HashMap<>();
+		pathVariables.put("id", id.toString());
+		Producto producto = clienteRest.getForObject("http://localhost:8001/ver/{id}", Producto.class, pathVariables);
+		return new Item(producto, cantidad);
 	}
 
 }
