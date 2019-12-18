0e29cfe7672a2dc2091142d3343132ce6d81c8e8
israel <israel.bejarano.a@gmail.com>
2019-12-18 18:15:07 +0100

    Leer configuración desde controlador utilizando anotación @Value

--- springboot-servicio-item/src/main/java/com/microservicios/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/microservicios/springboot/app/item/controllers/ItemController.java
@@ -1,9 +1,16 @@
 package com.microservicios.springboot.app.item.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +23,17 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 @RestController
 public class ItemController {
 	
+	private static Logger log = LoggerFactory.getLogger(ItemController.class);
+	
 	@Autowired
 	@Qualifier("serviceFeign")
 	private ItemService itemService;
+	
+	// este valor esta declarado en el fichero servicio-items.properties del folder config
+	// que es un repo local de git que es como el servidor Spring Cloud (config-server) obtiene las 
+	// posibles configuraciones
+	@Value("${configuracion.texto}")
+	private String texto;
 	
 	@GetMapping("/listar")
 	public List<Item> listar() {
@@ -40,6 +55,15 @@ public class ItemController {
 		producto.setPrecio(500.00);
 		item.setProducto(producto);
 		return item;
+	}
+	
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
+		log.info(texto);
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
 
 }
